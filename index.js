ff2b47f2653a7719aacc7646cb2d5ce7f8b6aa17
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-09-16 21:24:53 +0600

    created the basic server setup

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+// ! Required variables
+const app = express();
+const port = 5000;
+const uri = process.env.MONGODB_URI;
+
+// ! Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		const db = client.db('contactPilot');
+
+		// ! Database collections
+		const contactsCollection = db.collection('contacts');
+		const usersCollection = db.collection('users');
+
+		// Send a ping to confirm a successful connection
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} catch (error) {
+		console.log(`Error: ${error}`);
+	}
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
